bb4326b98b12931e8867ab7a90e8d2be4e1ff40f
Kevin Elliott <kelliott8046@jh-mba158-11.d214.org>
2018-04-26 15:01:51 -0500

    Touches Moved finished

--- pong/GameScene.swift
+++ pong/GameScene.swift
@@ -39,4 +39,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
            rightPaddle.run(SKAction.moveTo(y: -location.x, duration: 0.2))
            leftPaddle.run(SKAction.moveTo(y: location.x, duration: 0.2))
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        bottomPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+        leftPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+        rightPaddle.run(SKAction.moveTo(x: -location.x, duration: 0.2))
+        
+    }
 }
